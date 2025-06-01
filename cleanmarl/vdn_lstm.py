faa400c13bfd054a8cf52bcf0b72248b22f8c973
AndamAmine <andamamine83@gmail.com>
2025-06-01 21:58:36 +0100

    add an RNN verions

--- cleanmarl/vdn_lstm.py
+++ cleanmarl/vdn_lstm.py
@@ -17,27 +17,31 @@ from torch.utils.tensorboard import SummaryWriter
 
 @dataclass
 class Args:
-    env_type: str = "pz"
+    env_type: str = "smaclite"
     """ Pettingzoo, SMAClite ... """
-    env_name: str = "pursuit_v4"
+    env_name: str = "MMM"
     """ Name of the environment"""
     env_family: str ="sisl"
     """ Env family when using pz"""
     buffer_size: int = 5000
     """ The size of the replay buffer"""
+    seq_length: int = 10
+    """ Length of the sequence to store in the buffer"""
+    burn_in: int = 7
+    """Sequences to burn during batch updates"""
     total_timesteps: int = 2000000
+    """ Total steps in the environment during training"""
     gamma: float = 0.99
     """ Discount factor"""
-    """ Total steps in the environment during training"""
     learning_starts: int = 5000 
     """ Number of env steps to initialize the replay buffer"""
-    train_freq: int = 10
+    train_freq: int = 20
     """ Training frequency, relative to total_timesteps"""
     optimizer: str = "Adam"
     """ The optimizer"""
-    learning_rate: float =  0.000005
+    learning_rate: float =  0.00005
     """ Learning rate"""
-    batch_size: int = 64
+    batch_size: int = 10
     """Batch size"""
     start_e: float = 1
     """the starting epsilon for exploration"""
@@ -49,9 +53,9 @@ class Args:
     """ Hidden dimension"""
     num_layers: int = 2
     """ Number of layers"""
-    target_network_update_freq: int = 200
+    target_network_update_freq: int = 10000
     """ Frequency of updating target network"""
-    log_every: int = 10000
+    log_every: int = 1000
     """ Logging steps"""
     grad_clip: float =  4
     """grad clipping"""
@@ -71,35 +75,34 @@ class Args:
 class Qnetwrok(nn.Module):
     def __init__(self, input_dim,hidden_dim,num_layer,output_dim) -> None:
         super().__init__()
-        self.layers = nn.ModuleList()
-        self.layers.append(nn.Sequential(nn.Linear(input_dim, hidden_dim), nn.ReLU()))
-        for i in range(num_layer):
-            self.layers.append(
-                nn.Sequential(nn.Linear(hidden_dim, hidden_dim), nn.ReLU())
-            )
-        self.layers.append(nn.Sequential(nn.Linear(hidden_dim, output_dim)))
-        
-
+        self.hidden_dim = hidden_dim
+        self.fc1 = nn.Sequential(nn.Linear(input_dim, hidden_dim), nn.ReLU())
+        self.gru = nn.GRUCell(args.hidden_dim, hidden_dim)
+        self.fc2 = nn.Sequential(nn.Linear(hidden_dim, output_dim))        
     
-    def forward(self,x,mask=None):
-        for layer in self.layers:
-            x = layer(x)
+    def forward(self,x,h=None,mask=None):
+        x = self.fc1(x)
+        if h is None:
+            h = torch.zeros(x.size(0), self.hidden_dim, device=x.device)
+        h = self.gru(x,h)
+        x = self.fc2(h)
         if mask is not None:
             x = x.masked_fill(~mask, float('-inf'))
-        return x
+        return x,h
 class ReplayBuffer:
-    def __init__(self,buffer_size,num_agents,obs_space,action_space):
+    def __init__(self,buffer_size,num_agents,obs_space,action_space,seq_length):
         self.buffer_size = buffer_size
         self.num_agents = num_agents
         self.obs_space = obs_space
         self.action_space = action_space
+        self.seq_length = seq_length
 
-        self.obs = np.zeros((self.buffer_size,self.num_agents,self.obs_space))
-        self.mask = np.zeros((self.buffer_size,self.num_agents,self.action_space))
-        self.action = np.zeros((self.buffer_size,self.num_agents))
-        self.reward = np.zeros((self.buffer_size,1))
-        self.next_obs = np.zeros((self.buffer_size,self.num_agents,self.obs_space))
-        self.done = np.zeros((self.buffer_size,1))
+        self.obs = np.zeros((self.buffer_size,self.seq_length,self.num_agents,self.obs_space))
+        self.mask = np.zeros((self.buffer_size,self.seq_length,self.num_agents,self.action_space))
+        self.action = np.zeros((self.buffer_size,self.seq_length,self.num_agents))
+        self.reward = np.zeros((self.buffer_size,self.seq_length))
+        self.next_obs = np.zeros((self.buffer_size,self.seq_length,self.num_agents,self.obs_space))
+        self.done = np.zeros((self.buffer_size,self.seq_length))
         self.pos = 0
         self.size = 0
     def store(self,obs,action,reward,done,next_obs,mask):
@@ -169,27 +172,22 @@ if __name__ == "__main__":
                                           hidden_dim=args.hidden_dim,
                                           num_layer=args.num_layers,
                                           output_dim=env.get_action_size())
-    if not args.weight_sharing :
-        agents_utility_network = nn.ModuleList([agents_utility_network for _ in env.n_agents])
-        agents_target_network = nn.ModuleList([agents_target_network for _ in env.n_agents])
-    ## initialize the optimizer
+    
     optimizer = getattr(optim, args.optimizer) # get which optimizer to use from args
-    ### There is two approaches especially when the weights are not shared: 
-    ###one single optimizer (meaning shared optimizer states among the agents) or each one to have his own optimizer
     optimizer = optimizer(agents_utility_network.parameters(),lr = args.learning_rate)
 
-    if not args.weight_sharing and args.sep_optimizers:
-        optimizers = [
-                    optimizer(agents_utility_network[i].parameters(), lr=args.learning_rate)
-                    for i in range(env.n_agents) ]
-    ## initialize a shared replay buffer
     rb = ReplayBuffer(
         buffer_size=args.buffer_size,
         obs_space= env.get_obs_size(),
         action_space=env.get_action_size(),
-        num_agents= env.n_agents
+        num_agents= env.n_agents,
+        seq_length=args.seq_length
     )
     obs,_ = env.reset()
+    h = None
+    seq_to_rb = []
+    seq_obs, seq_actions,seq_reward,seq_done,seq_next_obs,seq_mask = [],[],[],[],[],[]
+    current_seq_len = 0
     mask = torch.tensor(env.get_avail_actions(), dtype=torch.bool, device=args.device)
     ep_reward = 0
     ep_length = 0
@@ -211,23 +209,31 @@ if __name__ == "__main__":
         epsilon = linear_schedule(args.start_e, args.end_e, args.exploration_fraction * args.total_timesteps, step)
         if random.random() < epsilon:
             actions = env.sample()
+            h = None
         else:
-            if args.weight_sharing:
-                ## instead of looping through the agents, we can see the number of the agents as a batch size (env.n_agents, shape_obs) ---> (batch_size, shape_obs)
-                obs = torch.from_numpy(obs).to(args.device)
-                q_values = agents_utility_network(obs,mask =mask)
-            else :
-                q_values = []
-                for agent_idx, network in enumerate(agents_utility_network): 
-                    q_value = network(obs[agent_idx],mask = mask[agent_idx])
-                q_values = torch.stack(q_values)
+            obs = torch.from_numpy(obs).to(args.device)
+            q_values,h = agents_utility_network(obs,h=h,mask =mask)
+            h = h.detach()
             actions  = torch.argmax(q_values,dim=-1)
         next_obs, reward, done, truncated, infos = env.step(actions)
         ep_reward += reward
         ep_length += 1
         
         mask = torch.tensor(env.get_avail_actions(), dtype=torch.bool, device=args.device)
-        rb.store(obs, actions,reward,done,next_obs,mask)
+
+        if current_seq_len >= args.seq_length:
+            current_seq_len = 0
+            rb.store(np.stack(seq_obs),np.stack( seq_actions),np.stack(seq_reward),np.stack(seq_done),np.stack(seq_next_obs),np.stack(seq_mask) )
+            seq_obs, seq_actions,seq_reward,seq_done,seq_next_obs,seq_mask = [],[],[],[],[],[]
+        seq_obs.append(obs)
+        seq_actions.append(actions)
+        seq_reward.append(reward)
+        seq_done.append(done)
+        seq_next_obs.append(next_obs)
+        seq_mask.append(mask)
+        obs = next_obs 
+        current_seq_len+= 1
+        
         if done or truncated:
             obs, _ = env.reset()
             mask = torch.tensor(env.get_avail_actions(), dtype=torch.bool, device=args.device)
@@ -237,6 +243,8 @@ if __name__ == "__main__":
                 ep_stats.append(infos)
             ep_reward = 0
             ep_length = 0
+            h = None
+
         if step % args.log_every == 0:
                 if len(ep_rewards) > 0: 
                     writer.add_scalar("rollout/ep_reward", np.mean(ep_rewards), step)
@@ -250,13 +258,32 @@ if __name__ == "__main__":
         if step > args.learning_starts:
             if step % args.train_freq: 
                 batch_obs,batch_action,batch_reward,batch_next_obs,batch_mask,batch_done = rb.sample(args.batch_size,normalize_reward= args.normalize_reward)
-                with torch.no_grad():
-                    q_next_max,_ = agents_target_network(batch_next_obs,mask =batch_mask ).max(dim=-1)
-                    vdn_q_max = q_next_max.sum(dim=-1)
-                    targets = batch_reward.squeeze() + args.gamma * (1-batch_done.squeeze())*vdn_q_max #(torch.sum(q_next_max, dim=-1))
-                q_values = torch.gather(agents_utility_network(batch_obs),dim=-1, index=batch_action.unsqueeze(-1)).squeeze()
-                vdn_q_values = q_values.sum(dim = -1)
-                loss = F.mse_loss(targets,vdn_q_values)
+                h_target = None
+                h_utility = None
+                for t in range(args.burn_in):
+                    batch_next_obs_t = batch_next_obs[:,t,:].reshape(args.batch_size*env.n_agents,-1)
+                    mask_t = batch_mask[:,t,:].reshape(args.batch_size*env.n_agents,-1)
+                    batch_obs_t = batch_obs[:,t,:].reshape(args.batch_size*env.n_agents,-1)
+                    _,h_target =  agents_target_network(batch_next_obs_t,h = h_target,mask =mask_t )
+                    _,h_utility = agents_utility_network(batch_obs_t,h=h_utility)
+                h_target = h_target.detach()
+                loss = 0
+                for t in range(args.burn_in, args.seq_length):
+                    with torch.no_grad():
+                        batch_next_obs_t = batch_next_obs[:,t,:].reshape(args.batch_size*env.n_agents,-1)
+                        mask_t = batch_mask[:,t,:].reshape(args.batch_size*env.n_agents,-1)
+                        q_next,h_target = agents_target_network(batch_next_obs_t,h=h_target,mask =mask_t )
+                        q_next = q_next.reshape(args.batch_size,env.n_agents,-1)
+                        q_next_max,_ = q_next.max(dim=-1)
+                        vdn_q_max = q_next_max.sum(dim=-1)
+                    
+                    targets = batch_reward[:,t].squeeze() + args.gamma * (1-batch_done[:,t].squeeze())*vdn_q_max
+                    batch_obs_t = batch_obs[:,t,:].reshape(args.batch_size*env.n_agents,-1)
+                    q_values,h_utility = agents_utility_network(batch_obs_t,h=h_utility)
+                    q_values = q_values.reshape(args.batch_size,env.n_agents,-1)
+                    q_values = torch.gather(q_values,dim=-1, index=batch_action[:,t,:].unsqueeze(-1)).squeeze()
+                    vqn_q_values = q_values.sum(dim = -1)
+                    loss += F.mse_loss(targets,vqn_q_values)
                 optimizer.zero_grad()
                 loss.backward()
                 grads = [p.grad for p in agents_utility_network.parameters() ]
@@ -265,7 +292,7 @@ if __name__ == "__main__":
                 torch.nn.utils.clip_grad_norm_(agents_utility_network.parameters(), max_norm=args.grad_clip, norm_type=2)
                 optimizer.step()
                 losses.append(loss.item())
-                q_vals.append(vdn_q_values.mean().item())
+                q_vals.append(vqn_q_values.mean().item())
                 if step % args.target_network_update_freq:
                     soft_update(
                         target_net=agents_target_network,
@@ -279,7 +306,7 @@ if __name__ == "__main__":
                 losses = []
                 q_vals = []
                 gradients = []
-        obs = next_obs 
+        
         
 
         if step % args.eval_steps == 0 and step > args.learning_starts:
@@ -290,14 +317,16 @@ if __name__ == "__main__":
             eval_ep_stats = []
             current_reward = 0
             current_ep_length = 0
+            h_eval = None
             while eval_ep < args.num_eval_ep:
                 eval_obs = torch.from_numpy(eval_obs).to(args.device)
                 mask_eval = torch.tensor(eval_env.get_avail_actions(), dtype=torch.bool, device=args.device)
-                q_values = agents_utility_network(eval_obs, mask = mask_eval)
+                q_values,h_eval = agents_utility_network(eval_obs,h=h_eval, mask = mask_eval)
                 actions  = torch.argmax(q_values,dim=-1)
                 next_obs_, reward, done, truncated, infos = eval_env.step(actions)
                 current_reward += reward
                 current_ep_length += 1
+                eval_obs = next_obs_
                 if done or truncated:
                     eval_obs, _ = eval_env.reset()
                     eval_ep_reward.append(current_reward)
@@ -306,7 +335,7 @@ if __name__ == "__main__":
                     current_reward = 0
                     current_ep_length = 0
                     eval_ep +=1
-                eval_obs = next_obs_
+                    h_eval = None
             writer.add_scalar("eval/ep_reward",np.mean(eval_ep_reward), step)
             writer.add_scalar("eval/std_ep_reward",np.std(eval_ep_reward), step)
             writer.add_scalar("eval/ep_length",np.mean(eval_ep_length), step)
