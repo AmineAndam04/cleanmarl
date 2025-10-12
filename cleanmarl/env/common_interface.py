a7945d29a22589795edf5fa5f8444027460fd166
AndamAmine <andamamine83@gmail.com>
2025-10-12 14:41:15 +0100

    update env and toml

--- cleanmarl/env/common_interface.py
+++ cleanmarl/env/common_interface.py
@@ -3,14 +3,20 @@
 
 
 class CommonInterface(object):
-    
     def step(self,actions):
         raise NotImplementedError
-    
-
     def reset(self):
         raise NotImplementedError
-    
+    def get_avail_actions(self):
+        raise NotImplementedError
+    def get_action_size(self):
+        raise NotImplementedError
+    def get_state(self):
+        raise NotImplementedError
+    def get_state_size(self):
+        raise NotImplementedError
+    def get_obs_size(self):
+        raise NotImplementedError
     def close(self):
         raise NotImplementedError
     def sample():
